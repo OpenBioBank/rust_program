a0a0237a37d1269d0a16cbfcd82bfba037ef7a92
Alain <zhengze01@gmail.com>
2024-03-31 22:31:01 +0800

    add create_new

--- src/processor.rs
+++ src/processor.rs
@@ -1,6 +1,8 @@
 use solana_program::{entrypoint::ProgramResult, rent::Rent, sysvar::Sysvar};
 
-pub fn create_new(
+pub fn create_new( 
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
     id: u64,
     description: String,
     owner: String,
@@ -8,6 +10,12 @@ pub fn create_new(
     authorize: bool,
     url: String,
 ) -> ProgramResult {
+    //解析账户
+let wallet_account="wallet_account";
+    //创建pda账户
+
+    //用program_id和钱包的acount
+
     // To create a new account in our plan we must:
     // Calculate the space and rent required for the account
     // Have an address to assign new accounts to
