3c10ed48ac5ef3d028e221eb6667aaabcdcb7031
alain <zhengze01@gmail>
2024-03-31 18:30:28 +0800

    add entrypoint.rs

---
+++ src/entrypoint.rs
@@ -0,0 +1,37 @@
+use crate::instruction::Instruction;
+
+use crate::processor::{authorize_account, create_new, delete, update_url};
+use solana_program::{
+    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, pubkey::Pubkey,
+};
+
+entrypoint!(process_instruction);
+
+fn process_instruction(
+    _program_id: &Pubkey,
+    _accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    // Parse instruction data into NftInstruction enumeration
+    // Call unpack to deserialize instruction_data
+    let instruction = Instruction::unpack(instruction_data)?;
+    // Match the returned data struct to what you expect
+    match instruction {
+        Instruction::Create {
+            id,
+            owner,
+            creator,
+            description,
+            authorize,
+            url,
+        } => {
+            create_new(id, owner, creator, description, authorize, url)?;
+        }
+
+        Instruction::Delete { id } => {
+            // Execute program code to delete a note
+            delete()?;
+        }
+    };
+    Ok(())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,5 @@
-///
+mod entrypoint;
 mod error;
 mod instruction;
 mod processor;
 mod state;
-
-use instruction::Instruction;
-
-use processor::{authorize_account, create_new, delete, update_url};
-use solana_program::{
-    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, pubkey::Pubkey,
-};
-
-entrypoint!(process_instruction);
-
-fn process_instruction(
-    _program_id: &Pubkey,
-    _accounts: &[AccountInfo],
-    instruction_data: &[u8],
-) -> ProgramResult {
-    // Parse instruction data into NftInstruction enumeration
-    // Call unpack to deserialize instruction_data
-    let instruction = Instruction::unpack(instruction_data)?;
-    // Match the returned data struct to what you expect
-    match instruction {
-        Instruction::Create {
-            id,
-            owner,
-            creator,
-            description,
-            authorize,
-            url,
-        } => {
-            create_new(id, owner, creator, description, authorize, url)?;
-        }
-
-        Instruction::Delete { id } => {
-            // Execute program code to delete a note
-            delete()?;
-        }
-    };
-    Ok(())
-}
